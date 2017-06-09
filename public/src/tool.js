3a1bac91f313398a964d975c28d72b7f1a5ff756
toni-york <toni.york@gmail.com>
2017-06-09 07:04:53 -0400

    Toni teleport testing take 71

--- public/src/tool.js
+++ public/src/tool.js
@@ -969,8 +969,7 @@ function createTeleMarker() {
 }
 function putTeleMarkerInCanvas(myGroupStr, myContext) {
 	// use true flag to tell putGroupInCanvas not to use the background rect
-	//putGroupInCanvas(myGroupStr, myContext, 212, 80, 60, 39, 0, 0, 60, 39, true)
-	putGroupInCanvas(myGroupStr, myContext, 0, 0, 60, 39, 0, 0, 60, 39, true)
+	putGroupInCanvas(myGroupStr, myContext, 212, 80, 180, 180, 0, 0, 180, 180, true)
 }
 function drawMarkersOnMap() {
 	// get current info out of localStorage
