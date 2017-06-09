71effc916421254b88f82ba7e4d5ade34d3ce12f
toni-york <toni.york@gmail.com>
2017-06-09 05:49:15 -0400

    Toni teleport testing take 33

--- public/src/tool.js
+++ public/src/tool.js
@@ -967,7 +967,7 @@ function createTeleMarker() {
 	putTeleMarkerInCanvas(teleMarkerImg, myCanvas.getContext("2d"));
 }
 function putTeleMarkerInCanvas(myGroupStr, myContext) {
-	putGroupInCanvas(myGroupStr, myContext, canvasWidth/3, canvasWidth/3, canvasWidth/3, canvasWidth/2, 0, 0, 50, 50)
+	putGroupInCanvas(myGroupStr, myContext, canvasWidth/3, canvasWidth/3, canvasWidth/3, canvasWidth/2, 0, 0, 100, 100)
 }
 function drawMarkersOnMap() {
 	// get current info out of localStorage
